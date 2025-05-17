6e15521f25ffb61c0eb20f0fe8484de6c6b29b3d
tsj2003 <121600765+tsj2003@users.noreply.github.com>
2025-05-17 20:47:18 +0530

    Fix: Update build configuration for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/personalportfolio-2' : '',
 }
 
 module.exports = nextConfig 
